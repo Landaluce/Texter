8c1f9786d3655cab9dfb9cb86f673de54b9d4e42
Landaluce <alvaro_delandaluce@whaetoncollege.edu>
2024-11-02 14:30:48 -0500

    initial commit

--- src/TextProcessor.py
+++ src/TextProcessor.py
@@ -25,15 +25,14 @@ class TextProcessor:
 
     @staticmethod
     def capitalize_sentences(text):
-        # List of sentence-ending punctuation
-        sentence_ending_punctuations = ['? ', '. ', '! ']
+        text = re.sub(r'(?<=[.!?]) +', '##SPLIT##', text)  # Split sentences
+        sentences = text.split('##SPLIT##')
 
-        # Iterate over each punctuation mark and capitalize accordingly
-        for punctuation in sentence_ending_punctuations:
-            if punctuation in text:
-                sentences = text.split(punctuation)
-                sentences = [sentence.capitalize() for sentence in sentences]
-                text = punctuation.join(sentences)  # Reassemble the text with the current punctuation
+        # Capitalize the first character of each sentence
+        sentences = [sentence.strip().capitalize() for sentence in sentences]
+
+        # Reassemble the text with proper sentence endings
+        return ' '.join(sentences)
 
 
         # I
@@ -41,13 +40,14 @@ class TextProcessor:
         # I'll
         # I've
         # I'd
-        return text
 
     @staticmethod
     def overlap_chunks(lst, n, stride=0):
         """Yield successive n-sized chunks from lst with stride length of overlap."""
         for i in range(0, len(lst), n - stride):
             yield lst[i:i + n]
+            if i + n >= len(lst):
+                break
 
     def predict(self, words):
         overlap = 5
@@ -57,31 +57,31 @@ class TextProcessor:
 
         batches = list(self.overlap_chunks(words, chunk_size, overlap))
 
-        # if the last batch is smaller than the overlap,
-        # we can just remove it
-        if len(batches[-1]) <= overlap:
-            batches.pop()
-
         tagged_words = []
         for batch in batches:
-            # use last batch completely
-            if batch == batches[-1]:
-                overlap = 0
             text = " ".join(batch)
             result = self.pipe(text)
-            assert len(text) == result[-1]["end"], "chunk size too large, text got clipped"
 
+            # We now keep track of where we are in the result
             char_index = 0
             result_index = 0
-            for word in batch[:len(batch) - overlap]:
-                char_index += len(word) + 1
-                # if any sub-token of a word is labeled as sentence end
-                # we label the whole word as sentence end
-                label = 0
-                while result_index < len(result) and char_index > result[result_index]["end"]:
-                    label = result[result_index]['entity']
-                    score = result[result_index]['score']
+            for word in batch:
+                # Update char_index for the word
+                char_index += len(word) + 1  # Account for the space after each word
+
+                # Determine the appropriate label and score
+                while (result_index < len(result) and
+                       char_index > result[result_index]["end"]):
                     result_index += 1
+
+                # Default label to "0" and score to None
+                label = "0"
+                score = 0.0
+
+                # If within bounds, get the label and score from the result
+                if result_index < len(result):
+                    label = result[result_index]["entity"]
+                    score = result[result_index]["score"]
 
                 tagged_words.append([word, label, score])
 
