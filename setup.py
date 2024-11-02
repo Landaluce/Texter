d29c138d3cf2a32db7d751cc59e6c57fb466d62f
Landaluce <alvaro_delandaluce@whaetoncollege.edu>
2024-11-02 11:49:47 -0500

    addad project name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='your_project_name',  # Replace with your project name
+    name='Texter',
     version='0.1',
     packages=find_packages(),
     install_requires=[
