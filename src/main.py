deab45ea2d405d0180ac9482a6bf887e32078b77
Landaluce <alvaro_delandaluce@whaetoncollege.edu>
2025-01-10 15:16:17 -0600

    minor fix

--- src/main.py
+++ src/main.py
@@ -5,25 +5,41 @@ from src.utils.speech_recognition_utils import *
 def main():
     """
     Initializes the application app_state and starts the live speech interpreter.
-    This function sets up the application by creating an instance of `AppState`, loading
-    the necessary commands, and initializing the speech recognizer. It then
-    begins the live speech interpretation process, which listens for and handles voice commands.
+    This function sets up the application by:
+    - Creating an instance of `AppState`
+    - Loading the necessary commands.
+    - Initializing the speech recognizer.
+    - Starting the live speech interpretation process in a separate thread.
+
+    Example Usage:
+        Run the script to start the application and speech recognition.
+
+    Raises:
+        RuntimeError: If commands cannot be loaded or threads fail to start.
     """
     command_files_directory = "speech_commands"
 
-    app = TexterUI(command_files_directory)
-    recognizer = sr.Recognizer()
-    app_state = AppState(app)
-    commands = get_commands(command_files_directory)
-    app_state.load_commands(commands)
+    try:
+        app = TexterUI(command_files_directory)
+        recognizer = sr.Recognizer()
+        app_state = AppState(app)
 
-    speech_thread = threading.Thread(
-        target=run_live_speech_interpreter, args=(app_state, app, recognizer)
-    )
-    app.speech_thread = speech_thread
-    speech_thread.start()
+        # Load commands
+        commands = get_commands(command_files_directory)
+        app_state.load_commands(commands)
 
-    app.init_ui(app_state, commands)
+        # Start live speech interpreter thread
+        speech_thread = threading.Thread(
+            target=run_live_speech_interpreter, args=(app_state, app, recognizer), daemon=True
+        )
+        app.speech_thread = speech_thread
+        speech_thread.start()
+
+        # Initialize UI
+        app.init_ui(app_state, commands)
+
+    except Exception as e:
+        raise
 
 
 if __name__ == "__main__":
