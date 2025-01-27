acb0a498cf5d56b65886022fbd7e446b6f332f6f
Landaluce <alvaro_delandaluce@whaetoncollege.edu>
2025-01-27 13:27:41 -0600

    added logging

--- src/utils/speech_recognition.py
+++ src/utils/speech_recognition.py
@@ -1,10 +1,15 @@
 from __future__ import annotations
 
+from typing import Optional
+
 import speech_recognition as sr
 import threading  # noqa: F401
+import logging
+from logging_config import setup_logging
+setup_logging()
 
 
-def recognize_speech(recognizer: sr.Recognizer, timeout: int = 2) -> str or None:
+def recognize_speech(recognizer: sr.Recognizer, timeout: int = 2) -> Optional[str]:
     """
     Recognizes and returns the speech from the given audio source using the specified recognizer.
 
@@ -20,18 +25,19 @@ def recognize_speech(recognizer: sr.Recognizer, timeout: int = 2) -> str or None
         try:
             audio = recognizer.listen(source, timeout=timeout)
             text = recognizer.recognize_google(audio).lower()
+            logging.info(f"voice input: {text}")
             return text
 
         except sr.UnknownValueError:
-            # print("Could not understand audio")
+            logging.warning("Could not understand audio (sr.UnknownValueError)")
             pass
         except sr.RequestError as e:
-            print(f"Error from Speech Recognition service: {e}")
+            logging.error(f"Error from Speech Recognition service: {e}")
             return None
         except sr.WaitTimeoutError:
-            # Suppress timeout error message and continue
-            pass
+            logging.error("waiting time error")
+            return None
         except Exception as e:
-            print(f"An unexpected error occurred: {e}")
+            logging.error(f"unexpected error: {e}")
             return None
     return None
